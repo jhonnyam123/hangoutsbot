d639dfff000032c0af7e893011363ef2a3312275
jhonnyam123 <2011norwegen@gmail.com>
2016-11-14 13:35:05 +0100

    Debugging

--- hangupsbot/plugins/telesync/__init__.py
+++ hangupsbot/plugins/telesync/__init__.py
@@ -240,7 +240,7 @@ def tg_on_message(tg_bot, tg_chat_id, msg):
             if telesync_config['sync_reply_to']:
                 if 'reply_to_message' in msg:
                     content_type, chat_type, chat_id = telepot.glance(msg['reply_to_message'])
-                    if msg['reply_to_message']['from']['first_name'].lower() == tg_bot.getMe():
+                    if msg['reply_to_message']['from']['first_name'].lower() == tg_bot.getMe().lower():
                         r_text = msg['reply_to_message']['text'].split(':') if 'text' in msg[
                             'reply_to_message'] else content_type
                         r2_user = r_text[0]
